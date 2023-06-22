834b9c464fca99f00d52c447ff1aaf0d82f6ee87
shoma0321 <s1540200102@toyo.jp>
2023-06-22 17:35:49 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -5,13 +5,11 @@ Bundler.require(*Rails.groups)
 
 module Nextapp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3000', 'https://technical-task-henna.vercel.app/'# ここにはクライアントのオリジンを指定します
-
+        origins 'localhost:3000',
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head]
